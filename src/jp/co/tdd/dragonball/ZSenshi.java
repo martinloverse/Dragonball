f031ffbe5609f42cb493285e10ba0daa624a05e1
Ikuo Suyama <martin-d28jp-love@i.softbank.jp>
2012-05-05 16:55:43 +0900

    ポイント１　リファクタ前

--- src/jp/co/tdd/dragonball/ZSenshi.java
+++ src/jp/co/tdd/dragonball/ZSenshi.java
@@ -2,8 +2,27 @@ package jp.co.tdd.dragonball;
 
 public class ZSenshi {
 
+    public static final String GOKU = "GOKU";
+    public static final String PIKKORO = "PIKKORO";
+    public static final String KURIRIN = "KURIRIN";
+
+    private int sentoryoku;
+
+    public ZSenshi(String name) {
+        if(name.equals(GOKU)) {
+            //悟空：フリーザ戦クリリンのことかー！時　ウィキペディアより
+            this.sentoryoku = 150000000;
+        } else if (name.equals(PIKKORO)) {
+            //ピッコロ：フリーザ戦時　ウィキペディアより
+            this.sentoryoku = 1800000;
+        } else if (name.equals(KURIRIN)) {
+            //クリリン：フリーザ戦時　ウィキペディアより
+            this.sentoryoku = 13000;
+        }
+    }
+
     public int getSentoryoku() {
-        return 5000;
+        return this.sentoryoku;
     }
 
 }
--- test/jp/co/tdd/dragonball/ZSenshiTest.java
+++ test/jp/co/tdd/dragonball/ZSenshiTest.java
@@ -5,13 +5,19 @@ import junit.framework.TestCase;
 public class ZSenshiTest extends TestCase {
 
     public void testSentoryoku() {
-        ZSenshi goku = new ZSenshi();
-        ZSenshi pikkoro = new ZSenshi();
-        ZSenshi kuririn = new ZSenshi();
+
+        ZSenshi goku = new ZSenshi(ZSenshi.GOKU);
+        ZSenshi pikkoro = new ZSenshi(ZSenshi.PIKKORO);
+        ZSenshi kuririn = new ZSenshi(ZSenshi.KURIRIN);
 
         assertNotNull("Z戦士は戦闘力を持つ", goku.getSentoryoku());
         assertNotNull("Z戦士は戦闘力を持つ", pikkoro.getSentoryoku());
         assertNotNull("Z戦士は戦闘力を持つ", kuririn.getSentoryoku());
+
+        assertTrue("ピッコロはクリリンより強い", pikkoro.getSentoryoku()
+                > kuririn.getSentoryoku());
+        assertTrue("悟空はピッコロよりスゲー強い", goku.getSentoryoku()
+                > pikkoro.getSentoryoku());
 
     }
 
