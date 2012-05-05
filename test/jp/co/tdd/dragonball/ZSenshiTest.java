07626cfb086aa074344bd0117b583e389e03e353
Ikuo Suyama <martin-d28jp-love@i.softbank.jp>
2012-05-05 15:53:01 +0900

    TODO1：Z戦士は皆“戦闘力“という共通の強さを測る値を持つ

--- test/jp/co/tdd/dragonball/ZSenshiTest.java
+++ test/jp/co/tdd/dragonball/ZSenshiTest.java
@@ -6,7 +6,13 @@ public class ZSenshiTest extends TestCase {
 
     public void testSentoryoku() {
         ZSenshi goku = new ZSenshi();
+        ZSenshi pikkoro = new ZSenshi();
+        ZSenshi kuririn = new ZSenshi();
+
         assertNotNull("Z戦士は戦闘力を持つ", goku.getSentoryoku());
+        assertNotNull("Z戦士は戦闘力を持つ", pikkoro.getSentoryoku());
+        assertNotNull("Z戦士は戦闘力を持つ", kuririn.getSentoryoku());
+
     }
 
 }
