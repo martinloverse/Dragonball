894767d3244b492429f6e859f4ba6d0e9dd78898
Ikuo Suyama <martin-d28jp-love@i.softbank.jp>
2012-05-05 15:50:31 +0900

    追加

--- test/jp/co/tdd/dragonball/ZSenshiTest.java
+++ test/jp/co/tdd/dragonball/ZSenshiTest.java
@@ -4,4 +4,9 @@ import junit.framework.TestCase;
 
 public class ZSenshiTest extends TestCase {
 
+    public void testSentoryoku() {
+        ZSenshi goku = new ZSenshi();
+        assertNotNull("Z戦士は戦闘力を持つ", goku.getSentoryoku());
+    }
+
 }
